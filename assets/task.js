3c44e23836f29c4b83b090a27e913d495ea10d9e
Markotupanjac <markotupanjac@gmail.com>
2014-05-17 16:22:08 -0500

    Advancing the random generator

--- assets/task.js
+++ assets/task.js
@@ -3,9 +3,19 @@
         var wordGen = Math.random();
         var url_setter = document.getElementById('url-setter'), url = document.getElementById('url'), the_iframe = document.getElementById('the-frame');
         if(wordGen < 0.01){
-	        url.value = "http://net.net";
-        } if(wordGen > 0.5){
-	        url.value="http://hello.com";
+	        url.value = "http://en.wikipedia.org/wiki/Adult";
+        }else if(0.01< wordGen < 0.02){
+	        url.value="http://en.wikipedia.org/wiki/Aeroplane";
+        }else if(0.02< wordGen < 0.03){
+	        url.value="http://en.wikipedia.org/wiki/Air";
+        }else if(0.03< wordGen < 0.04){
+	        url.value="http://en.wikipedia.org/wiki/Aircraft";
+        }else if(0.04< wordGen < 0.05){
+	        url.value="http://en.wikipedia.org/wiki/Airforce";
+        }else if(0.05< wordGen < 0.06){
+	        url.value="http://en.wikipedia.org/wiki/Airport";
+        }else if(wordGen > 0.1){
+	        url.value="http://en.wikipedia.org/wiki/Airport";
         }
         
         
@@ -13,7 +23,7 @@
         
         
         
-        url_setter.onload = function (event) {
+        url_setter.onsubmit = function (event) {
             event.preventDefault();
             the_iframe.src = url.value;
         };
