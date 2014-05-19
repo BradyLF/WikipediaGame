10746bcfbc4cd51e16d1e6780f6eae71b6a3367f
Brady <BradyLF@users.noreply.github.com>
2014-05-19 10:05:49 -0500

    Update victory.js

--- assets/victory.js
+++ assets/victory.js
@@ -1,6 +1,6 @@
 function victory(){
 	var url = document.getElementById('the-frame').src;
-	if(url.value.substring(29, 41) == dest.value) {
+	if(url.substring(29, 41) == dest.value) {
 		window.alert("Congratulations, You Made It To Your Destination");
     	}
 }
