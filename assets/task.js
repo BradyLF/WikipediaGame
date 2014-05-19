f7a3c0bd4459193cdfd7391219641c23732e8418
Markotupanjac <markotupanjac@gmail.com>
2014-05-18 19:42:56 -0500

    Restructured Options, and now it tells you if you win

--- assets/task.js
+++ assets/task.js
@@ -3,210 +3,211 @@
         var wordGen = Math.random();
         var url_setter = document.getElementById('url-setter'), url = document.getElementById('url'), the_iframe = document.getElementById('the-frame');
         if(wordGen < 0.01){
-	        url.value = "http://en.wikipedia.org/wiki/Adult";
+	        url.value = "Adult";
         }else if(0.01< wordGen < 0.02){
-	        url.value="http://en.wikipedia.org/wiki/Aeroplane";
+	        url.value="Aeroplane";
         }else if(0.02< wordGen < 0.03){
-	        url.value="http://en.wikipedia.org/wiki/Air";
+	        url.value="Air";
         }else if(0.03< wordGen < 0.04){
-	        url.value="http://en.wikipedia.org/wiki/Aircraft";
+	        url.value="Aircraft";
         }else if(0.04< wordGen < 0.05){
-	        url.value="http://en.wikipedia.org/wiki/Airforce";
+	        url.value="Airforce";
         }else if(0.05< wordGen < 0.06){
-	        url.value="http://en.wikipedia.org/wiki/Airport";
+	        url.value="Airport";
         }else if(0.06< wordGen < 0.07){
-	        url.value="http://en.wikipedia.org/wiki/Album";
+	        url.value="Album";
         }else if(0.07< wordGen < 0.08){
-	        url.value="http://en.wikipedia.org/wiki/Alphabet";
+	        url.value="Alphabet";
         }else if(0.08< wordGen < 0.09){
-	        url.value="http://en.wikipedia.org/wiki/Apple";
+	        url.value="Apple";
         }else if(0.09< wordGen < 0.1){
-	        url.value="http://en.wikipedia.org/wiki/Arm";
+	        url.value="Arm";
         }else if(0.1< wordGen < 0.11){
-	        url.value="http://en.wikipedia.org/wiki/Army";
+	        url.value="Army";
         }else if(0.11< wordGen < 0.12){
-	        url.value="http://en.wikipedia.org/wiki/Baby";
+	        url.value="Baby";
         }else if(0.12< wordGen < 0.13){
-	        url.value="http://en.wikipedia.org/wiki/Backpack";
+	        url.value="Backpack";
         }else if(0.13< wordGen < 0.14){
-	        url.value="http://en.wikipedia.org/wiki/Balloon";
+	        url.value="Balloon";
         }else if(0.14< wordGen < 0.15){
-	        url.value="http://en.wikipedia.org/wiki/Banana";
+	        url.value="Banana";
         }else if(0.15< wordGen < 0.16){
-	        url.value="http://en.wikipedia.org/wiki/Bank";
+	        url.value="Bank";
         }else if(0.16< wordGen < 0.17){
-	        url.value="http://en.wikipedia.org/wiki/Barbecue";
+	        url.value="Barbecue";
         }else if(0.17< wordGen < 0.18){
-	        url.value="http://en.wikipedia.org/wiki/Bathroom";
+	        url.value="Bathroom";
         }else if(0.18< wordGen < 0.19){
-	        url.value="http://en.wikipedia.org/wiki/Bathtub";
+	        url.value="Bathtub";
         }else if(0.19< wordGen < 0.2){
-	        url.value="http://en.wikipedia.org/wiki/Bed";
+	        url.value="Bed";
         }else if(0.2< wordGen < 0.21){
-	        url.value="http://en.wikipedia.org/wiki/Bee";
+	        url.value="Bee";
         }else if(0.21< wordGen < 0.22){
-	        url.value="http://en.wikipedia.org/wiki/Bible";
+	        url.value="Bible";
         }else if(0.22< wordGen < 0.23){
-	        url.value="http://en.wikipedia.org/wiki/Bird";
+	        url.value="Bird";
         }else if(0.23< wordGen < 0.24){
-	        url.value="http://en.wikipedia.org/wiki/Bomb";
+	        url.value="Bomb";
         }else if(0.24< wordGen < 0.25){
-	        url.value="http://en.wikipedia.org/wiki/Book";
+	        url.value="Book";
         }else if(0.25< wordGen < 0.26){
-	        url.value="http://en.wikipedia.org/wiki/Needle";
+	        url.value="Needle";
         }else if(0.26< wordGen < 0.27){
-	        url.value="http://en.wikipedia.org/wiki/Paint";
+	        url.value="Paint";
         }else if(0.27< wordGen < 0.28){
-	        url.value="http://en.wikipedia.org/wiki/Cup";
+	        url.value="Cup";
         }else if(0.28< wordGen < 0.29){
-	        url.value="http://en.wikipedia.org/wiki/Computer";
+	        url.value="Computer";
         }else if(0.29< wordGen < 0.3){
-	        url.value="http://en.wikipedia.org/wiki/Crystal";
+	        url.value="Crystal";
         }else if(0.3< wordGen < 0.31){
-	        url.value="http://en.wikipedia.org/wiki/Finger";
+	        url.value="Finger";
         }else if(0.31< wordGen < 0.32){
-	        url.value="http://en.wikipedia.org/wiki/Fire";
+	        url.value="Fire";
         }else if(0.32< wordGen < 0.33){
-	        url.value="http://en.wikipedia.org/wiki/Junk";
+	        url.value="Junk";
         }else if(0.33< wordGen < 0.34){
-	        url.value="http://en.wikipedia.org/wiki/Man";
+	        url.value="Man";
         }else if(0.34< wordGen < 0.35){
-	        url.value="http://en.wikipedia.org/wiki/Meat";
+	        url.value="Meat";
         }else if(0.35< wordGen < 0.36){
-	        url.value="http://en.wikipedia.org/wiki/Monster";
+	        url.value="Monster";
         }else if(0.36< wordGen < 0.37){
-	        url.value="http://en.wikipedia.org/wiki/Roof";
+	        url.value="Roof";
         }else if(0.37< wordGen < 0.38){
-	        url.value="http://en.wikipedia.org/wiki/Salt";
+	        url.value="Salt";
         }else if(0.38< wordGen < 0.39){
-	        url.value="http://en.wikipedia.org/wiki/Pepper";
+	        url.value="Pepper";
         }else if(0.39< wordGen < 0.4){
-	        url.value="http://en.wikipedia.org/wiki/Train";
+	        url.value="Train";
         }else if(0.4< wordGen < 0.41){
-	        url.value="http://en.wikipedia.org/wiki/Space";
+	        url.value="Space";
         }else if(0.41< wordGen < 0.42){
-	        url.value="http://en.wikipedia.org/wiki/Moon";
+	        url.value="Moon";
         }else if(0.42< wordGen < 0.43){
-	        url.value="http://en.wikipedia.org/wiki/Sun";
+	        url.value="Sun";
         }else if(0.43< wordGen < 0.44){
-	        url.value="http://en.wikipedia.org/wiki/Planet";
+	        url.value="Planet";
         }else if(0.44< wordGen < 0.45){
-	        url.value="http://en.wikipedia.org/wiki/Star";
+	        url.value="Star";
         }else if(0.45< wordGen < 0.46){
-	        url.value="http://en.wikipedia.org/wiki/Stomach";
+	        url.value="Stomach";
         }else if(0.46< wordGen < 0.47){
-	        url.value="http://en.wikipedia.org/wiki/Web";
+	        url.value="Web";
         }else if(0.47< wordGen < 0.48){
-	        url.value="http://en.wikipedia.org/wiki/Sword";
+	        url.value="Sword";
         }else if(0.48< wordGen < 0.49){
-	        url.value="http://en.wikipedia.org/wiki/Torch";
+	        url.value="Torch";
         }else if(0.49< wordGen < 0.5){
-	        url.value="http://en.wikipedia.org/wiki/Pyramid";
+	        url.value="Pyramid";
         }else if(0.5< wordGen < 0.51){
-	        url.value="http://en.wikipedia.org/wiki/Rocket";
+	        url.value="Rocket";
         }else if(0.51< wordGen < 0.52){
-	        url.value="http://en.wikipedia.org/wiki/Prison";
+	        url.value="Prison";
         }else if(0.52< wordGen < 0.53){
-	        url.value="http://en.wikipedia.org/wiki/Pillow";
+	        url.value="Pillow";
         }else if(0.53< wordGen < 0.54){
-	        url.value="http://en.wikipedia.org/wiki/Elephant";
+	        url.value="Elephant";
         }else if(0.54< wordGen < 0.55){
-	        url.value="http://en.wikipedia.org/wiki/Animal";
+	        url.value="Animal";
         }else if(0.55< wordGen < 0.56){
-	        url.value="http://en.wikipedia.org/wiki/Parachute";
+	        url.value="Parachute";
         }else if(0.56< wordGen < 0.57){
-	        url.value="http://en.wikipedia.org/wiki/Money";
+	        url.value="Money";
         }else if(0.57< wordGen < 0.58){
-	        url.value="http://en.wikipedia.org/wiki/Meteor";
+	        url.value="Meteor";
         }else if(0.58< wordGen < 0.59){
-	        url.value="http://en.wikipedia.org/wiki/Perfume";
+	        url.value="Perfume";
         }else if(0.59< wordGen < 0.6){
-	        url.value="http://en.wikipedia.org/wiki/Ice";
+	        url.value="Ice";
         }else if(0.6< wordGen < 0.61){
-	        url.value="http://en.wikipedia.org/wiki/Water";
+	        url.value="Water";
         }else if(0.61< wordGen < 0.62){
-	        url.value="http://en.wikipedia.org/wiki/Sport";
+	        url.value="Sport";
         }else if(0.62< wordGen < 0.63){
-	        url.value="http://en.wikipedia.org/wiki/Navy";
+	        url.value="Navy";
         }else if(0.63< wordGen < 0.64){
-	        url.value="http://en.wikipedia.org/wiki/Grapes";
+	        url.value="Grapes";
         }else if(0.64< wordGen < 0.65){
-	        url.value="http://en.wikipedia.org/wiki/Girl";
+	        url.value="Girl";
         }else if(0.65< wordGen < 0.66){
-	        url.value="http://en.wikipedia.org/wiki/Gas";
+	        url.value="Gas";
         }else if(0.66< wordGen < 0.67){
-	        url.value="http://en.wikipedia.org/wiki/Boy";
+	        url.value="Boy";
         }else if(0.67< wordGen < 0.68){
-	        url.value="http://en.wikipedia.org/wiki/Fruit";
+	        url.value="Fruit";
         }else if(0.68< wordGen < 0.69){
-	        url.value="http://en.wikipedia.org/wiki/Car";
+	        url.value="Car";
         }else if(0.69< wordGen < 0.7){
-	        url.value="http://en.wikipedia.org/wiki/Earth";
+	        url.value="Earth";
         }else if(0.7< wordGen < 0.71){
-	        url.value="http://en.wikipedia.org/wiki/Diamond";
+	        url.value="Diamond";
         }else if(0.71< wordGen < 0.72){
-	        url.value="http://en.wikipedia.org/wiki/Electricity";
+	        url.value="Electricity";
         }else if(0.72< wordGen < 0.73){
-	        url.value="http://en.wikipedia.org/wiki/Circus";
+	        url.value="Circus";
         }else if(0.73< wordGen < 0.74){
-	        url.value="http://en.wikipedia.org/wiki/Alphabet";
+	        url.value="Alphabet";
         }else if(0.74< wordGen < 0.75){
-	        url.value="http://en.wikipedia.org/wiki/Comet";
+	        url.value="Comet";
         }else if(0.75< wordGen < 0.76){
-	        url.value="http://en.wikipedia.org/wiki/Light";
+	        url.value="Light";
         }else if(0.76< wordGen < 0.77){
-	        url.value="http://en.wikipedia.org/wiki/Color";
+	        url.value="Color";
         }else if(0.77< wordGen < 0.78){
-	        url.value="http://en.wikipedia.org/wiki/Science";
+	        url.value="Science";
         }else if(0.78< wordGen < 0.79){
-	        url.value="http://en.wikipedia.org/wiki/Life";
+	        url.value="Life";
         }else if(0.79< wordGen < 0.8){
-	        url.value="http://en.wikipedia.org/wiki/Comic";
+	        url.value="Comic";
         }else if(0.8< wordGen < 0.81){
-	        url.value="http://en.wikipedia.org/wiki/Television";
+	        url.value="Television";
         }else if(0.81< wordGen < 0.82){
-	        url.value="http://en.wikipedia.org/wiki/Torpedo";
+	        url.value="Torpedo";
         }else if(0.82< wordGen < 0.83){
-	        url.value="http://en.wikipedia.org/wiki/Vampire";
+	        url.value="Vampire";
         }else if(0.83< wordGen < 0.84){
-	        url.value="http://en.wikipedia.org/wiki/Weapon";
+	        url.value="Weapon";
         }else if(0.84< wordGen < 0.85){
-	        url.value="http://en.wikipedia.org/wiki/Toilet";
+	        url.value="Toilet";
         }else if(0.85< wordGen < 0.86){
-	        url.value="http://en.wikipedia.org/wiki/Tennis";
+	        url.value="Tennis";
         }else if(0.86< wordGen < 0.87){
-	        url.value="http://en.wikipedia.org/wiki/Spice";
+	        url.value="Spice";
         }else if(0.87< wordGen < 0.88){
-	        url.value="http://en.wikipedia.org/wiki/Music";
+	        url.value="Music";
         }else if(0.88< wordGen < 0.89){
-	        url.value="http://en.wikipedia.org/wiki/School";
+	        url.value="School";
         }else if(0.89< wordGen < 0.9){
-	        url.value="http://en.wikipedia.org/wiki/Language";
+	        url.value="Language";
         }else if(0.9< wordGen < 0.91){
-	        url.value="http://en.wikipedia.org/wiki/Cell";
+	        url.value="Cell";
         }else if(0.91< wordGen < 0.92){
-	        url.value="http://en.wikipedia.org/wiki/Botany";
+	        url.value="Botany";
         }else if(0.92< wordGen < 0.93){
-	        url.value="http://en.wikipedia.org/wiki/Family";
+	        url.value="Family";
         }else if(0.93< wordGen < 0.94){
-	        url.value="http://en.wikipedia.org/wiki/Time";
+	        url.value="Time";
         }else if(0.94< wordGen < 0.95){
-	        url.value="http://en.wikipedia.org/wiki/Weather";
+	        url.value="Weather";
         }else if(0.95< wordGen < 0.96){
-	        url.value="http://en.wikipedia.org/wiki/Computer_programming";
+	        url.value="Computer_programming";
         }else if(0.96< wordGen < 0.97){
-	        url.value="http://en.wikipedia.org/wiki/Clothes";
+	        url.value="Clothes";
         }else if(0.97< wordGen < 0.98){
-	        url.value="http://en.wikipedia.org/wiki/Egypt";
+	        url.value="Egypt";
         }else if(0.98< wordGen < 0.99){
-	        url.value="http://en.wikipedia.org/wiki/America";
-        }else if(0.99< wordGen < 1.00){
-	        url.value="http://en.wikipedia.org/wiki/Fun";
+	        url.value="America";
+        }else if(0.99< wordGen <= 1.00){
+	        url.value="Fun";
         }
         window.onload = function(){
 	        url_setter();
 	        }
                 url_setter = function () {
-            the_iframe.src = url.value;
+            the_iframe.src ="http://en.wikipedia.org/wiki/"+ url.value;
             };
     }());
+    
