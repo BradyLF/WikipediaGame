ca3c2930fd39908faa1bee74059b428a80e30392
Markotupanjac <markotupanjac@gmail.com>
2014-05-18 20:20:17 -0500

    More Stuff

--- assets/task.js
+++ assets/task.js
@@ -206,5 +206,8 @@
         window.onload = function(){
 	        url_setter();
 	        }
-                    }());
+                url_setter = function () {
+            the_iframe.src ="http://en.wikipedia.org/wiki/"+ url.value;
+            };
+    }());
     
